8d1d320201ce0f25a768002b4ee577d492b13a7f
Pavlo Matviienko <pavelmatvienkomatvi@gmail.com>
2025-05-13 16:32:29 +0300

    push

--- template-page/contact.php
+++ template-page/contact.php
@@ -6,7 +6,7 @@ get_header();
 ?>
 <main>
   <?php
-    get_template_part("template-parts/sections/contact");
+    get_template_part("template-parts/sections/contact", null, ["page" => true]);
   ?>
 </main>
 <?php get_footer(); ?>
