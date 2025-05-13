1c0263f1714e94ebf74e4ecc013a970c288a3af0
Pavlo Matviienko <pavelmatvienkomatvi@gmail.com>
2025-05-13 18:31:04 +0300

    push

--- header.php
+++ header.php
@@ -4,7 +4,7 @@
     <meta charset="<?php bloginfo('charset'); ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="profile" href="http://gmpg.org/xfn/11">
-    <title><?php get_the_document_title(); ?></title>
+    
     <?php wp_head(); ?>
 </head>
 <body <?php body_class(); ?>>
