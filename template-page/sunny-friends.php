164ad1fb9f1e319fd8a87df7f7367826f0d101fe
Pavlo Matviienko <pavelmatvienkomatvi@gmail.com>
2025-05-15 18:41:24 +0300

    push

--- template-page/sunny-friends.php
+++ template-page/sunny-friends.php
@@ -4,7 +4,11 @@
  */
   get_header();
   $tabs = get_field('sunny_friends_tabs');
-  $percent = 100 / count($tabs);
+  $percent = 100;
+  $notEmpty = count($tabs) !== 0;
+  if($notEmpty) {
+    $percent = 100 / count($tabs);
+  }
 ?>
 <main>
   <section class="sunny-friends-section">
@@ -28,36 +32,40 @@
       <div class="sunny-friends-table-section__top">
         <div class="sunny-friends-table-section__buttons" style="<?= "--offset-width: $percent%"; ?>">
           <?php
-            $i = 0;
-            foreach($tabs as $button) {
-              $class = $i === 0 ? ' active' : '';
-              $name = $button['sunny_friends_tab_name'];
-              echo "<button data-index='$i' class='sunny-friends-table-section__button$class'>$name</button>";
-              $i++;
-            }
+            if($notEmpty) {
+              $i = 0;
+              foreach($tabs as $button) {
+                $class = $i === 0 ? ' active' : '';
+                $name = $button['sunny_friends_tab_name'];
+                echo "<button data-index='$i' class='sunny-friends-table-section__button$class'>$name</button>";
+                $i++;
+              };
+            };
           ?>
         </div>
         <div class="sunny-friends-table-section__tabs">
           <?php
-            $i = 0;
-            foreach($tabs as $tab) {
-              $class = $i === 0 ? ' active' : '';
-              echo "<div data-index='$i' class='sunny-friends-table-section__tab$class'>";
-              $content = $tab['sunny_friends_tab_content'];
-              foreach($content as $item) {
-                $image = $item['sunny_friends_tab_content_image'];
-                $url = isset($image['url']) ? $image['url'] : null;
-                $title = isset($image['title']) ? $image['title'] : null;
-                $text = $item['sunny_friends_tab_content_text'];
-                if($url && $title) {
-                  echo "<div class='item'>
-                    <img src='$url' alt='$title'>
-                    <span>$text</span>
-                  </div>";
-                }
+            if($notEmpty) {
+              $i = 0;
+              foreach($tabs as $tab) {
+                $class = $i === 0 ? ' active' : '';
+                echo "<div data-index='$i' class='sunny-friends-table-section__tab$class'>";
+                $content = $tab['sunny_friends_tab_content'];
+                foreach($content as $item) {
+                  $image = $item['sunny_friends_tab_content_image'];
+                  $url = isset($image['url']) ? $image['url'] : null;
+                  $title = isset($image['title']) ? $image['title'] : null;
+                  $text = $item['sunny_friends_tab_content_text'];
+                  if($url && $title) {
+                    echo "<div class='item'>
+                      <img src='$url' alt='$title'>
+                      <span>$text</span>
+                    </div>";
+                  }
+                };
+                echo '</div>';
+                $i++;
               };
-              echo '</div>';
-              $i++;
             };
           ?>
         </div>
