19fa17141806a644e245fefd9356dd5a74bc3dd3
Pavlo Matviienko <pavelmatvienkomatvi@gmail.com>
2025-05-16 16:52:16 +0300

    push

--- template-parts/gallery/gallery-grid.php
+++ template-parts/gallery/gallery-grid.php
@@ -18,20 +18,20 @@
   //         }
   //     }
   // };
-  $usedTermsArray = [];
-  if(!empty($masters)) {
-    foreach ($masters as $master) {
-      $images = get_field('master_images_work', $master->ID);
-      if (!is_array($images)) continue;
-      foreach ($images as $image) {
-        $tag = $image["master_image_work_tag"] ?? null;
-        if ($tag instanceof WP_Term && !isset($usedTerms[$tag->term_id])) {
-          $usedTerms[$tag->term_id] = $tag;
-        }
-      }
-    }
-    $usedTermsArray = array_values($usedTerms);
-  }
+  // $usedTermsArray = [];
+  // if(!empty($masters)) {
+  //   foreach ($masters as $master) {
+  //     $images = get_field('master_images_work', $master->ID);
+  //     if (!is_array($images)) continue;
+  //     foreach ($images as $image) {
+  //       $tag = $image["master_image_work_tag"] ?? null;
+  //       if ($tag instanceof WP_Term && !isset($usedTerms[$tag->term_id])) {
+  //         $usedTerms[$tag->term_id] = $tag;
+  //       }
+  //     }
+  //   }
+  //   $usedTermsArray = array_values($usedTerms);
+  // }
 ?>
 <div class="gallery-modal">
   <button type="button" aria-label="Close" class="cross"></button>
