8dc84a467ed362f2cf9858bd6a9c3227f9cf6163
Pavlo Matviienko <pavelmatvienkomatvi@gmail.com>
2025-05-19 17:54:00 +0300

    push

--- template-page/sunny-friends.php
+++ template-page/sunny-friends.php
@@ -9,7 +9,6 @@
   if($notEmpty) {
     $percent = 100 / count($tabs);
   };
-  $column = [ null, null, null, null, null ];
 ?>
 <main>
   <section class="sunny-friends-section">
@@ -74,34 +73,44 @@
       <div class="sunny-friends-table-section__wrapper">
         <div class="sunny-friends-table-section__table">
           <div class="column main">
-            <div class="cell name">Sunny Membership</div>
-            <div class="cell">Qualification Criteria</div>
-            <div class="cell">Qualification Criteria</div>
-            <div class="cell">Qualification Criteria</div>
-            <div class="cell">Qualification Criteria</div>
-            <div class="cell">Qualification Criteria</div>
+            <?php
+              $title = get_field('sunny_friends_table_title');
+              $items = get_field('sunny_friends_main_column_table');
+              echo "<div class='cell name'>$title</div>";
+              foreach($items as $item) {
+                $text = $item['sunny_friends_main_column_table_text'];
+                echo "<div class='cell'>$text</div>";
+              };
+            ?>
           </div>
           <?php
+            $columns = get_field("sunny_friends_table_column");
             $baseColor = '253, 196, 31';
-            $totalColumns = count($column);
+            $totalColumns = count($columns);
             $i = 0;
-            foreach ($column as $tab) {
+            foreach ($columns as $column) {
               $alpha = 0.1 + ($i / max($totalColumns - 1, 1)) * 0.9;
               $alpha = round($alpha, 2) + 0.1;
+              $title = $column['sunny_friends_table_column_title'];
+              $fields = $column['sunny_friends_table_column_field'];
+              $content = "";
+              foreach($fields as $field) {
+                $text = $field['sunny_friends_table_column_text'];
+                $isCheckClass = $field['sunny_friends_table_column_check_mark'] ? " check" : "";
+                $content .= "<div class='cell$isCheckClass'>$text</div>";
+              }
               echo "<div class='column'>
                 <div class='cell label'>
-                  <span style='background-color: rgba($baseColor, $alpha);'>Row</span>
+                  <span style='background-color: rgba($baseColor, $alpha);'>$title</span>
                 </div>
-                <div class='cell'>1 visit</div>
-                <div class='cell'>0%</div>
-                <div class='cell'>1 visit</div>
-                <div class='cell check'></div>
+                $content
               </div>";
               $i++;
             }
           ?>
         </div>
       </div>
+      <a href="#" class="btn yellow">Join Sunny Friends</a>
     </div>
   </section>
 </main>
