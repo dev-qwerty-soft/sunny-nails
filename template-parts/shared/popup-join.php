07fa61a28c8ac251e5cd5054a0179d1afffe5993
Pavlo Matviienko <pavelmatvienkomatvi@gmail.com>
2025-05-20 17:01:10 +0300

    push

---
+++ template-parts/shared/popup-join.php
@@ -0,0 +1,15 @@
+<div id="popupJoin" class="popup-join">
+  <button type="button" aria-label="Close" class="cross"></button>
+  <?php
+    $title = get_field('form_title', 'option');
+    if($title) {
+      echo "<h2 class='title'>$title</h2>";
+    };
+  ?>
+  <?php
+    $code = get_field('form_code', 'option');
+    if($code) {
+      echo do_shortcode($code);
+    };
+  ?>
+</div>
