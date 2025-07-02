be32404278360e83778205040e646cce5a1203af
OleksandrVasylchuk <sanyabatyushka@gmail.com>
2025-07-02 19:14:24 +0300

    Add footer menu to the footer section and remove duplicate menu code
    - Added a conditional check to display the footer menu if it exists.
    - Removed duplicate footer menu code from the footer bottom section.

--- footer.php
+++ footer.php
@@ -2,15 +2,16 @@
   <div class="container">
     <div class="footer__top">
       <?= logo('footer_logo'); ?>
+      <?php if (has_nav_menu('footer-menu')) { ?>
+        <?php wp_nav_menu(array('theme_location' => 'footer-menu', 'depth' => 1, 'menu_class' => 'footer__menu')); ?>
+      <?php } ?>
       <div class="icons">
         <?php displayIcon(); ?>
       </div>
     </div>
     <div class="footer__bottom">
       <p class="copyright"><?= get_field('footer_copyright', 'option'); ?></p>
-      <?php if (has_nav_menu('footer-menu')) { ?>
-        <?php wp_nav_menu(array('theme_location' => 'footer-menu', 'depth' => 1, 'menu_class' => 'footer__menu')); ?>
-      <?php } ?>
+
       <?php if (has_nav_menu('footer-menu-aside')) { ?>
         <?php wp_nav_menu(array('theme_location' => 'footer-menu-aside', 'depth' => 1, 'menu_class' => 'footer__menu__aside')); ?>
       <?php } ?>
@@ -19,4 +20,5 @@
 </footer>
 <?php wp_footer(); ?>
 </body>
+
 </html>
