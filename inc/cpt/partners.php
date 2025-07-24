d2dd2b1be7d8a65be5638b6eb5fe3d574e35dce4
OleksandrVasylchuk <sanyabatyushka@gmail.com>
2025-07-24 20:26:09 +0300

    Add custom post type and template for Partners section
    - Created a new custom post type 'partner' with support for title and thumbnail.
    - Registered a taxonomy 'partner_cat' for categorizing partners.
    - Developed a new template 'partners.php' to display partner information, including hero section, benefits, and partner cards.
    - Implemented filtering functionality for partners based on categories.
    - Added popup functionality for detailed partner information.

---
+++ inc/cpt/partners.php
@@ -0,0 +1,20 @@
+<?php
+add_action('init', function () {
+    register_post_type('partner', [
+        'labels' => [
+            'name' => 'Partners',
+            'singular_name' => 'Partner',
+        ],
+        'public' => true,
+        'has_archive' => false,
+        'rewrite' => false,
+        'show_in_rest' => true,
+        'supports' => ['title', 'thumbnail'],
+        'menu_icon' => 'dashicons-groups',
+    ]);
+    register_taxonomy('partner_cat', 'partner', [
+        'label' => 'Categories',
+        'hierarchical' => true,
+        'show_in_rest' => true,
+    ]);
+});
