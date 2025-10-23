d65dc81ace15e06bd79a99818edf3e298faf9d47
Pavlo Matviienko <pavelmatvienkomatvi@gmail.com>
2025-10-23 18:29:53 +0300

    push

--- header.php
+++ header.php
@@ -3,7 +3,7 @@
 
 <head>
     <meta charset="<?php bloginfo('charset'); ?>">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
     <link rel="profile" href="http://gmpg.org/xfn/11">
     <meta name="robots" content="index, follow">
     <meta name="application-name" content="SUNNY NAILS">
